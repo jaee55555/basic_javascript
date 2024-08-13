1c830b16b3572ddb741927ee2d23fa605ee9e8ae
jaee55555 <jaeeO55555@gmail.com>
2024-08-13 20:28:46 +0900

    what is let and const

---
+++ Section_1/lecture_2.js
@@ -0,0 +1,12 @@
+let userInfo;
+let isTrueOrFalse;
+
+let phone = 'samsung';
+phone = 'apple';
+
+console.log(phone);
+
+const name = '윤재';
+// name = 'younjae';
+// console.log(name);
+const EMAIL_ADDRESS = "123@gmail.com"
