ced0c748282f99fbf26f649bf03b2f01be810576
jaee55555 <jaeeO55555@gmail.com>
2024-08-27 20:10:20 +0900

    what is js Module System

---
+++ Section_7/lecture_2/src/index.mjs
@@ -0,0 +1,7 @@
+// import {num, add, User} from './test.mjs';
+// import * as testModule from './test.mjs';
+import testModule from './test.mjs';
+
+// console.log(testModule.num);
+// console.log(testModule.add(10,19));
+console.log(new testModule('윤재'));
---
+++ Section_7/lecture_2/src/test.mjs
@@ -0,0 +1,15 @@
+// const num = 35;
+
+// function add(num1, num2){
+//     return num1+num2;
+// }
+
+function User(name){
+    this.name = name;
+}
+
+
+// export {num, add, User};
+export default User;
+
+//default를 사용하면 const, let, var는 내보낼 수 없다.
