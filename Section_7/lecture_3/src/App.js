418ee7f2bf15b41930fce4a84353d094a0ec72ac
jaee55555 <jaeeO55555@gmail.com>
2024-08-27 20:24:25 +0900

    Component

---
+++ Section_7/lecture_3/src/App.js
@@ -0,0 +1,7 @@
+import TabBar from "./component/TabBar";
+import Content from "./component/Content";
+
+export default function App($app){
+    const tabBar = new TabBar();
+    const content = new Content();
+}
---
+++ Section_7/lecture_3/src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+
+const $app = document.getElementById('app');
+
+//생성자 함수 코드
+new App($app);
